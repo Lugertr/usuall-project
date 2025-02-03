ff02fdd35591a888c792a654659aec7ff1bd8a53
Вишневский Никита <vishnevskiy.n2@wb.ru>
2025-02-04 01:22:35 +0300

    add enviroment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  storeInfo: {
+    token: null,
+    user_email: null,
+    user_name: null,
+    user_id: null,
+    token2: null,
+    email_confirmed: null,
+    token3: null,
+    insales_id: null,
+  },
+};
