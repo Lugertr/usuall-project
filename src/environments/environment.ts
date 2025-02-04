2b9556affbe89338c60f1d20094d61d1f00920cd
Вишневский Никита <vishnevskiy.n2@wb.ru>
2025-02-04 13:42:22 +0300

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,15 @@
+import 'dotenv/config';
+
 export const environment = {
   production: false,
   storeInfo: {
-    token: process.env.STORE_TOKEN || '',
-    user_email: process.env.STORE_USER_EMAIL || '',
-    user_name: process.env.STORE_USER_NAME || '',
-    user_id: Number(process.env.STORE_USER_ID) || 0,
-    token2: process.env.STORE_TOKEN2 || '',
-    email_confirmed: process.env.STORE_EMAIL_CONFIRMED === 'true',
-    token3: process.env.STORE_TOKEN3 || '',
-    insales_id: Number(process.env.STORE_INSALES_ID) || 0
+    token: import.meta.env.NG_APP_TOKEN || '',
+    user_email: import.meta.env.NG_APP_USER_EMAIL || '',
+    user_name: import.meta.env.NG_APP_USER_NAME || '',
+    user_id: Number(import.meta.env.NG_APP_USER_ID) || 0,
+    token2: import.meta.env.NG_APP_TOKEN2 || '',
+    email_confirmed: import.meta.env.NG_APP_EMAIL_CONFIRMED === 'true',
+    token3: import.meta.env.NG_APP_TOKEN3 || '',
+    insales_id: Number(import.meta.env.NG_APP_INSALES_ID) || 0
   }
 };
