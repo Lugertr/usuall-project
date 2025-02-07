9574d7a7f3cf4642f8f67b835112f2e2453fbfde
Вишневский Никита <vishnevskiy.n2@wb.ru>
2025-02-07 12:04:34 +0300

    fix auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { authGuard } from '@core/auth/auth.guard';
 
 export const routes: Routes = [
   { path: 'auth', loadComponent: () => import('./components/auth/login.component').then(c => c.LoginComponent) },
-  { path: 'main', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent), canActivate: [authGuard] },
-  { path: '**', redirectTo: 'main' }
+  { path: 'main', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent), },
+  { path: '**', redirectTo: 'auth' }
 ];
